70491c1bc598ad0f80febb11468a45bc7170c589
Danial <d.bayati28@gmail.com>
2023-04-09 16:18:16 +0430

    feat: create Site model and connect to related tables

--- api/models.py
+++ api/models.py
@@ -1,10 +1,28 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+
+class Site(models.Model):
+    STATUS_PENDING = 'P'
+    STATUS_ACTIVE = 'A'
+    STATUS = [
+        ('P', 'pending'),
+        ('A', 'active')
+    ]
+    url = models.CharField(max_length=1024)
+    status = models.CharField(max_length=1, choices=STATUS, default=STATUS_PENDING)
+    user = models.ManyToManyField(User, related_name='site', blank=True)
+
+    def __str__(self):
+        return f"{self.status} -- {self.url}"
+
 
 
 class Subdomain(models.Model):
     base = models.CharField(max_length=255)
     subdomain = models.CharField(max_length=511)
     ip = models.CharField(max_length=63)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
 
 
 class SecureSocketsLayersCertificate(models.Model):
@@ -19,6 +37,7 @@ class SecureSocketsLayersCertificate(models.Model):
     certiver = models.CharField(max_length=63)
     certialgo = models.CharField(max_length=63)
     expired = models.CharField(max_length=63)
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
 
 
 class urlsUnchecked(models.Model):
@@ -29,11 +48,14 @@ class urlsUnchecked(models.Model):
 class urlsChecked(models.Model):
     userID = models.CharField(max_length=31)
     url = models.CharField(max_length=127)
-    
+
+
 class BrokenUrls(models.Model):
     site_url = models.CharField(max_length=31)
     broken_link = models.CharField(max_length=127)
-    
+    site = models.ForeignKey(Site, on_delete=models.CASCADE, null=True)
+
+
 class UrlChecked(models.Model):
     url = models.CharField(max_length=31)
     hits = models.IntegerField()
